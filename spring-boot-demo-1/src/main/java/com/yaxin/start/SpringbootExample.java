0f99d4c220e3c7df7a129ffdaf122a5d1fb120cd
liruimin <1162908494@qq.com>
2018-01-31 15:28:34 +0800

    加入freemarker模板

--- spring-boot-demo-1/src/main/java/com/yaxin/start/SpringbootExample.java
+++ spring-boot-demo-1/src/main/java/com/yaxin/start/SpringbootExample.java
@@ -1,18 +1,27 @@
 package com.yaxin.start;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import com.yaxin.start.pojo.User;
+import com.yaxin.start.service.UserService;
 /**
  * 
  * @author liruimin
- * @SpringBootApplication开启了Spring的组件扫描和Spring Boot的自动配置功能。
- * 实际上它将三个注解组合在一起。分别是@Configuration、@ComponentScan、@EnableAutoConfiguration。
- * 在使用的过程中，我们应该将springboot的启动类放在最外层
+ * 
  */
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringbootExample {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+	
 		SpringApplication.run(SpringbootExample.class, args);
+	
 	}
 }
--- spring-boot-demo-1/src/main/java/com/yaxin/start/controller/IndexController.java
+++ spring-boot-demo-1/src/main/java/com/yaxin/start/controller/IndexController.java
@@ -1,14 +1,24 @@
 package com.yaxin.start.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 @RequestMapping(value="/index")
 public class IndexController {
-	
+	private Logger log = LoggerFactory.getLogger(IndexController.class);
+	  
 	@RequestMapping(value="/test1")
-	public String testController1(){
-		return "hello world!!!";
+	public String testController1(ModelMap map){
+		log.info("===============test controller===start=================");
+		map.addAttribute("name", "***");
+		map.addAttribute("bookTitle", "月光宝盒");
+		log.info("==========================end==========================");
+		return "welcome";
 	}
 }
